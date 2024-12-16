ac5a3f8fe65a3894f53ba87d5cd8e3a367a5829f
Blood1Gun <githabakkaunt@gmail.com>
2024-12-16 11:31:20 +0300

    Update Lab3 Bratskiy D.A..py

--- Lab3 Bratskiy D.A..py
+++ Lab3 Bratskiy D.A..py
@@ -1,7 +1,9 @@
-# Лаб работа №3
-# Братский Дмитрий, 100502-УБТа-о23, Вариант 2
+# # Лаб. работа №3
+# # Братский Дмитрий, 100502-УБТа-о23, Вариант 2
 
+# С помощью цикла for
 import math
+
 x1 = 0
 xn = 5
 dx = 0.5
@@ -18,3 +20,24 @@ for x in range(per):
 
 for x, y in results:
     print(f"x = {x:.2f}, y = {y:.4f}")
+
+
+# С помощью цикла While
+import numpy as np
+
+x1 = 0
+xn = 5
+dx = 0.5
+a = 0.5
+b = 0.7
+
+results = []
+
+x = x1
+while x <= xn:
+    y = np.sin(a * x) + 3 * (np.cos(b * x**2 + 1))**2
+    results.append((x, y))
+    x += dx
+
+for x, y in results:
+    print(f"x = {x:.1f}, y = {y:.4f}")
